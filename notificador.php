96028608e175b56744fbdbd408866fc69ddfc388
ocastelblanco <ocastelblanco@gmail.com>
2014-04-23 21:43:58 -0500

    Inclusion de textos oficiales de rechazo/aceptacion en el sistema de notificacion de docentes

--- notificador.php
+++ notificador.php
@@ -9,11 +9,21 @@ if (isset($_GET['id'])) {
 	$query = "SELECT `nombres`, `apellidos`, `email`, `nombreIP`, `estado` FROM `registrodocentes` WHERE `id` =".$_GET['id'];
 	$resultado = $mysqli->query($query);
 	$fila = $resultado->fetch_assoc();
+	$caracteres = array("á","é","í","ó","ú","ü","ñ","Á","É","Í","Ó","Ú","Ü","Ñ");
+	$reemplazos = array("a","e","i","o","u","u","n","A","E","I","O","U","U","N");
+	$nombres = str_replace($caracteres, $reemplazos, $fila['nombres']);
+	$apellidos = str_replace($caracteres, $reemplazos, $fila['apellidos']);
 	
+	$textoRechazoHTML = "$nombres $apellidos, reciba un cordial saludo.<br><br>Queremos agradecerle por su inscripci&oacute;n en el curso Ciudadan&iacute;as, nuestro equipo valora genuinamente su inter&eacute;s.<br><br>Como se plante&oacute; desde el inicio de este proceso, cada uno de los cuatro pa&iacute;ses (Argentina, Uruguay, Bolivia y Colombia) seleccion&oacute; a 20 personas para participar en el curso. Desafortunadamente usted no ha sido elegido para participar en esta corte del curso, pero reconociendo su excelente perfil y capacidad pedag&oacute;gica lo tendremos en cuenta para futuras oportunidades.<br><br>Nuevamente gracias por su inter&eacute;s.<br><br>Equipo Curso Ciudadan&iacute;as";
+	$textoRechazo = "$nombres $apellidos, reciba un cordial saludo. Queremos agradecerle por su inscripcion en el curso Ciudadanias, nuestro equipo valora genuinamente su interes. Como se planteo desde el inicio de este proceso, cada uno de los cuatro paises (Argentina, Uruguay, Bolivia y Colombia) selecciono a 20 personas para participar en el curso. Desafortunadamente usted no ha sido elegido para participar en esta corte del curso, pero reconociendo su excelente perfil y capacidad pedagogica lo tendremos en cuenta para futuras oportunidades. Nuevamente gracias por su interes. Equipo Curso Ciudadanias";
+	$textoAceptacionHTML = "$nombres $apellidos, reciba un cordial saludo.<br><br>Queremos agradecerle por su inscripci&oacute;n en el curso Ciudadan&iacute;as, nuestro equipo valora genuinamente su inter&eacute;s. De igual forma, queremos felicitarlo pues ha sido seleccionado por su pa&iacute;s para formar parte del grupo de 20 personas que participar&aacute;n en el curso Ciudadan&iacute;as.<br><br>Esta selecci&oacute;n se realiz&oacute; teniendo en cuenta su perfil e iniciativa pedag&oacute;gica por lo que para nosotros como equipo del curso ser&aacute; un gusto poder construir aprendizajes con usted y los otros participantes alrededor de los diferentes temas que nos interesan.<br><br>Pr&oacute;ximamente recibir&aacute; la informaci&oacute;n general del curso, la p&aacute;gina donde se ubicar&aacute;, su usuario y contrase&ntilde;a.<br><br>Adicionalmente, como este es un curso donde en la parte inicial puede seleccionar dos m&oacute;dulos para desarrollar, le pedimos que revise el siguiente listado y nos haga saber a qu&eacute; m&oacute;dulos desea inscribirse escribi&eacute;ndonos un mensaje con los nombres de los m&oacute;dulos de su inter&eacute;s al correo <a href='mailto:cursociudadanias@gmail.com'>cursociudadanias@gmail.com</a>:<br><ul><li>Convivencia escolar</li><li>G&eacute;nero</li><li>Medio Ambiente</li><li>Derechos Humanos</li><li>Prevenci&oacute;n del acoso escolar</li></ul><br><br>Nuevamente gracias por su inter&eacute;s y esperamos construir un excelente equipo de trabajo juntos.<br><br>Equipo Curso Ciudadan&iacute;as";
+	$textoAceptacion = "$nombres $apellidos, reciba un cordial saludo. Queremos agradecerle por su inscripcion en el curso Ciudadanias, nuestro equipo valora genuinamente su interes. De igual forma, queremos felicitarlo pues ha sido seleccionado por su pais para formar parte del grupo de 20 personas que participaran en el curso Ciudadanias. Esta seleccion se realizo teniendo en cuenta su perfil e iniciativa pedagogica por lo que para nosotros como equipo del curso sera un gusto poder construir aprendizajes con usted y los otros participantes alrededor de los diferentes temas que nos interesan. Proximamente recibira la informacion general del curso, la pagina donde se ubicara, su usuario y clave. Adicionalmente, como este es un curso donde en la parte inicial puede seleccionar dos modulos para desarrollar, le pedimos que revise el siguiente listado y nos haga saber a que modulos desea inscribirse escribiendonos un mensaje con los nombres de los modulos de su interes al correo cursociudadanias@gmail.com: 1) Convivencia escolar 2)Genero 3)Medio Ambiente 4)Derechos Humanos y 5)Prevencion del acoso escolar. Nuevamente gracias por su interes y esperamos construir un excelente equipo de trabajo juntos. Equipo Curso Ciudadanias";
 	if ($fila['estado'] == "1") {
-		$textoMensaje = "ha sido aceptada";
+		$textoMensajeHTML = $textoAceptacionHTML;
+		$textoMensaje = $textoAceptacion;
 	} else if ($fila['estado'] == "2") {
-		$textoMensaje = "ha sido rechazada";
+		$textoMensajeHTML = $textoRechazoHTML;
+		$textoMensaje = $textoRechazo;
 	}
 	
 	//SMTP needs accurate times, and the PHP time zone MUST be set
@@ -50,16 +60,16 @@ if (isset($_GET['id'])) {
 	//Set an alternative reply-to address
 	$mail->addReplyTo('femcidi@redeaprender.org', 'Sistema de registro FEMCIDI');
 	//Set who the message is to be sent to
-	$mail->addAddress($fila['email'], $fila['nombres'].' '.$fila['apellidos']);
+	$mail->addAddress($fila['email'], "$nombres $apellidos");
 	$mail->isHTML(true);  
 	//Set the subject line
-	$mail->Subject = 'Respuesta desde el sistema de registro FEMCIDI';
+	$mail->Subject = 'Registro de Iniciativas Pedagogicas para el Curso virtual Ciudadanias';
 	//Read an HTML message body from an external file, convert referenced images to embedded,
 	//convert HTML into a basic plain-text alternative body
 	//$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
-	$mail->msgHTML('Su iniciativa <strong>'.$fila['nombreIP'].'</strong> '.$textoMensaje.'.');
+	$mail->msgHTML($textoMensajeHTML);
 	//Replace the plain text body with one created manually
-	$mail->AltBody = 'Su iniciativa '.$fila['nombreIP'].' '.$textoMensaje.'.';
+	$mail->AltBody = $textoMensaje;
 	//Attach an image file
 	//$mail->addAttachment('img/ciudadanias.png');
 	sleep(15);
